11760131ba91210e180e3bcf0596d67cab8d82d6
madumas <madumas@madumas.ca>
2020-01-22 13:37:00 -0500

    keep infura for front end

--- src/eth/maker.js
+++ src/eth/maker.js
@@ -4,7 +4,7 @@ import McdPlugin from '@makerdao/dai-plugin-mcd'
 export default async function createMaker(network = 'mainnet') {
   return Maker.create('http',
       {
-          url: 'ws://vpn.0xna.me:8546/', //'ws://192.168.0.23:8546'//'https://mainnet.infura.io/v3/e37b6b6fede24263907500a81762f2ca',
+          url: 'https://mainnet.infura.io/v3/e37b6b6fede24263907500a81762f2ca', //'ws://vpn.0xna.me:8546/', //'ws://192.168.0.23:8546'//,
           plugins: [
               [McdPlugin, {}] // the second argument can be used to pass options to the plugin
           ],
